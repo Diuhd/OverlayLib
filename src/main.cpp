fe9deab3cc25d10c46fdc5d9c38a2c6d401274d0
Diuhd <philparks714@gmail.com>
2026-04-22 19:51:21 +0900

    added argparse to meson

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include "overlay.h"
 
+#include "argparse/argparse.hpp"
+
 #include <utility>
 
 int main(int argc, char** argv) {
-    OverlayConfig config{"io.diuhd.overlaylib", "samples/index.html"};
-    config.window()
-        .set_fullscreen(true)
-        .set_passthrough(true);
-    config.webview().set_developer_mode(false);
+    argparse::ArgumentParser program("ovl");
 
-    Overlay overlay{std::move(config)};
-    return overlay.run(argc, argv);
+    program.add_argument("")
+    // OverlayConfig config{"io.diuhd.overlaylib", "samples/index.html"};
+    // config.window()
+    //     .set_fullscreen(true)
+    //     .set_passthrough(true);
+    // config.webview().set_developer_mode(false);
+
+    // Overlay overlay{std::move(config)};
+    // return overlay.run(argc, argv);
 }
